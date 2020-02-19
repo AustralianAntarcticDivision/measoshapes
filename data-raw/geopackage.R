7daed0370601df1aacbf75b9e25a06e9d4d8d44f
Michael Sumner <mdsumner@gmail.com>
2020-02-19 14:51:01 +1100

    export geopackages

---
+++ data-raw/geopackage.R
@@ -0,0 +1,5 @@
+library(sf)
+shapes <- dplyr::inner_join(measoshapes::measo_regions05, measoshapes::measo_names)
+sf::st_write(shapes, "inst/extdata/measo_regions.gpkg")
+shapes_ll <- dplyr::inner_join(measoshapes::measo_regions05_ll, measoshapes::measo_names)
+sf::st_write(shapes_ll, "inst/extdata/measo_regions_ll.gpkg")
