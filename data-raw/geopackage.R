eceea041a8c17eb405bf110ea81ca11802c4e136
Michael Sumner <mdsumner@gmail.com>
2023-11-29 23:08:44 +0000

    gpkg in 3031

--- data-raw/geopackage.R
+++ data-raw/geopackage.R
@@ -3,3 +3,6 @@ shapes <- dplyr::inner_join(measoshapes::measo_regions05, measoshapes::measo_nam
 sf::st_write(shapes, "inst/extdata/measo_regions.gpkg")
 shapes_ll <- dplyr::inner_join(measoshapes::measo_regions05_ll, measoshapes::measo_names)
 sf::st_write(shapes_ll, "inst/extdata/measo_regions_ll.gpkg")
+
+## for Quantarctica (?)
+sf::write_sf(sf::st_transform(measoshapes::measo_regions05, "EPSG:3031"), "inst/extdata/measo_regions_epsg_3031.gpkg")
